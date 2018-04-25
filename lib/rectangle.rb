672aed663aa473eedd2eaa39e657b31e3b7c087d
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-25 17:16:46 +0100

    refactor: extract private build_top_row and build_bottom_row methods

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,7 +1,24 @@
 class Rectangle
 
+  TOP_LEFT_CORNER = "\u231C"
+  TOP_RIGHT_CORNER = "\u231D"
+  BOTTOM_LEFT_CORNER = "\u231E"
+  BOTTOM_RIGHT_CORNER = "\u231F"
+  NEW_LINE = "\n"
+
+
   def printer(width, height)
-    print "\u231C\u231D\n\u231E\u231F"
+    print "#{build_top_row}#{NEW_LINE}#{build_bottom_row}"
+  end
+
+  private
+
+  def build_top_row
+    "#{TOP_LEFT_CORNER}#{TOP_RIGHT_CORNER}"
+  end
+
+  def build_bottom_row
+    "#{BOTTOM_LEFT_CORNER }#{BOTTOM_RIGHT_CORNER}"
   end
 
 end
