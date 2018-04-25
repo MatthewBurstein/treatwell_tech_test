b11a232861f87e9fe4e98e8b8fb865191d3da270
Matt Burstein <j.matthew.burstein@gmail.com>
2018-04-25 17:53:57 +0100

    feat: prints rectangle height 2 and arbitrary width

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -4,21 +4,24 @@ class Rectangle
   TOP_RIGHT_CORNER = "\u231D"
   BOTTOM_LEFT_CORNER = "\u231E"
   BOTTOM_RIGHT_CORNER = "\u231F"
+  HORIZONTAL_EDGE = "-"
   NEW_LINE = "\n"
 
 
   def printer(width, height)
-    print "#{build_top_row}#{NEW_LINE}#{build_bottom_row}"
+    print "#{build_top_row(width)}#{NEW_LINE}#{build_bottom_row(width)}"
   end
 
   private
 
-  def build_top_row
-    "#{TOP_LEFT_CORNER}#{TOP_RIGHT_CORNER}"
+  def build_top_row(width)
+    edge_characters = HORIZONTAL_EDGE * (width - 2)
+    TOP_LEFT_CORNER + edge_characters + TOP_RIGHT_CORNER
   end
 
-  def build_bottom_row
-    "#{BOTTOM_LEFT_CORNER }#{BOTTOM_RIGHT_CORNER}"
+  def build_bottom_row(width)
+    edge_characters = HORIZONTAL_EDGE * (width - 2)
+    BOTTOM_LEFT_CORNER  + edge_characters + BOTTOM_RIGHT_CORNER
   end
 
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -5,8 +5,13 @@ describe Rectangle do
 
   describe ".print" do
     it "prints  2 x 2 square" do
-      square = "\u231C\u231D\n\u231E\u231F"
-      expect{ rectangle.printer(2, 2) }.to output(square).to_stdout
+      test_square = "\u231C\u231D\n\u231E\u231F"
+      expect{ rectangle.printer(2, 2) }.to output(test_square).to_stdout
+    end
+
+    it "prints a 3 x 2 rectangle" do
+      test_rectangle = "\u231C-\u231D\n\u231E-\u231F"
+      expect{ rectangle.printer(3, 2) }.to output(test_rectangle).to_stdout
     end
   end
 end
